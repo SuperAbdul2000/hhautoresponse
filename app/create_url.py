9deeccb1c45d3047879a29e08bc33ca0a775e677
SuperAbdul2000 <krasnovgleb0@gmail.com>
2026-05-09 02:55:04 +0400

    Add comments

--- app/create_url.py
+++ app/create_url.py
@@ -2,6 +2,7 @@ from urllib.parse import urlencode
 
 BASE_URL = "https://hh.ru/search/vacancy"
 
+#обязательные парамметры, которые я не изменяю для постройки базового url с вакансиями
 DEFAULT_PARAMS = {
     "salary": "",
     "ored_clusters": "true",
@@ -9,6 +10,7 @@ DEFAULT_PARAMS = {
     "hhtmFromLabel": "vacancy_search_list",
 }
 
+#функция для построения url с default парамметрами, с параметрами, которые мы изменяем в config и текущей страницей
 def build_search_url(custom_params: dict, page: int = 0) -> str:
     params = {**DEFAULT_PARAMS, **custom_params, "page": page,}
     
