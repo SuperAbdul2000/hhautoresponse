b1a5431971a3ea4c39fb94869f32ca9fb14685bb
SuperAbdul2000 <krasnovgleb0@gmail.com>
2026-05-09 03:00:33 +0400

    Add comments

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+#функция для считывания txt файла с сопроводительным письмом
 def load_cover_letter():
     return Path("app/cover_letter.txt").read_text(
         encoding="utf-8"
